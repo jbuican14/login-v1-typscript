66e206ee9cd6fd47111b8c17ee9d073acb3c4216
Juti Buican <juti.buican@bottomline.com>
2022-09-29 07:03:07 +0100

    TS with expressJS: Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, {Request, Response} from 'express'; 
+
+const app = express(); 
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+  <div>
+    <h1>Hello</h1>
+  </div>
+  `)
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
